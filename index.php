e995ad6ba227d4f3f385e4716cb5d3f9c6274d66
Lucien Pochon <lucien.pochon@heig-vd.ch>
2018-10-28 19:41:57 +0100

    Select tasks are order by date

--- index.php
+++ index.php
@@ -68,6 +68,7 @@ switch($_POST['action']) {
 /**
  * Select all tasks from the database.
  */
+
 $selectQuery = 'SELECT * FROM todo ORDER BY created_at DESC'; 
 $items = $db->query($selectQuery);
 ?>
